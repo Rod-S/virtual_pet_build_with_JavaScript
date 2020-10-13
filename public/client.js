fd87ec710f32eaa251e6c5ac06e03dfee9ed5681
Rod-S <rodsanati@gmail.com>
2020-10-12 21:05:28 -0500

    add ability for user to enter dog name; add weight message text

--- public/client.js
+++ public/client.js
@@ -2,10 +2,14 @@ $(function() {
 
     // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
     let pet_info = {
-        name: "Spot",
+        name: "",
         weight: 1,
+        weight_message: "",
         happiness: 0
     }
+
+    pet_info.name = prompt("What's the name of your dog?")
+
     // Called function to update the name, happiness, and weight of our pet in our HTML
     checkAndUpdatePetInfoInHtml();
 
@@ -15,7 +19,15 @@ $(function() {
     $('.exercise-button').click(clickedExerciseButton);
 
 
-
+    function petWeight() {
+        let pet_weight = 6;
+        if (pet_weight > 10) {
+            console.log("Your pet is obese.")
+        } else {
+            console.log("Your pet is in great shape.")
+        }
+    }
+    petWeight();
 
     function clickedTreatButton() {
         // Increase pet happiness
@@ -55,12 +67,18 @@ $(function() {
         if (pet_info.happiness < 1) {
             pet_info.happiness = 0;
         }
+        if (pet_info.weight > 10) {
+            pet_info.weight_message = "Oh, No! Your pet is obese!"
+        } else {
+            pet_info.weight_message = "Your pet is in great shape!"
+        }
     }
 
     // Updates your HTML with the current values in your pet_info object
     function updatePetInfoInHtml() {
         $('.name').text(pet_info.name);
         $('.weight').text(pet_info.weight);
+        $('.weight').append(`<p>${pet_info.weight_message}</p>`);
         $('.happiness').text(pet_info.happiness);
     }
 })
